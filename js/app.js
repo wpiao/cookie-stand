c0a6e531c3ece40d0169ea2f485e700a1464eeea
Wenhao Piao <wenhaopiao520@gmail.com>
2021-01-30 16:54:33 -0800

    Update hours array

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 'use strict';
 
 // Define global variables
-const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Location Total'];
+const hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'Daily Location Total'];
 const allLocations = [];
 
 // Lab-07: JS Constructor
